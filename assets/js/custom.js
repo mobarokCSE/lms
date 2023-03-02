811b4c8f4b7d1d6dd2dfc75207d17964ee0089d2
mobarok <mubarokbd3@gmail.com>
2023-03-02 22:10:00 +0600

    update home3 and fix home 1 issues

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -247,6 +247,11 @@ $(window).on('scroll', function () {
         fixed_top.removeClass("header-fixed fadeInUp");
     }
 });
+
+
+
+
+
 // =================== header js end here ===================
 
 
@@ -327,24 +332,24 @@ const Swiper4 = new Swiper('.course__slider--style4', {
 
 // home 1 testimonial  slider here
 
-// var swiperAuthor = new Swiper(".testimonial__author", {
-//     spaceBetween: 1,
-//     slidesPerView: 5,
-//     freeMode: true,
-//     watchSlidesProgress: true,
-//     // clickable: true,
-// });
+var swiperAuthor = new Swiper(".testimonial__author", {
+    spaceBetween: 1,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+    // clickable: true,
+});
 
 
-// const Swiper2 = new Swiper('.testimonial__slider', {
-//     grabCursor: true,
-//     loop: true,
-//     slidesPerView: 1,
-//     speed: 3000,
-//     thumbs: {
-//         swiper: swiperAuthor,
-//     },
-// });
+const Swiper2 = new Swiper('.testimonial__slider', {
+    grabCursor: true,
+    loop: true,
+    slidesPerView: 1,
+    speed: 3000,
+    thumbs: {
+        swiper: swiperAuthor,
+    },
+});
 
 
 
@@ -413,13 +418,12 @@ const Swiper7 = new Swiper('.testimonial__slider2', {
     spaceBetween: 24,
     grabCursor: true,
     loop: true,
-    slidesPerView: 2,
     breakpoints: {
         576: {
             slidesPerView: 1,
         },
         768: {
-            slidesPerView: 1,
+            slidesPerView: 2,
         },
         992: {
             slidesPerView: 2,
@@ -442,19 +446,20 @@ const Swiper7 = new Swiper('.testimonial__slider2', {
 
 
 // counter
-function counter(id, start, end, duration) {
-    let element = document.getElementById(id);
-    let range = end - start;
-    let current = start;
-    let increment = end > start ? 1 : -1;
-    let stepTime = Math.abs(Math.floor(duration / range));
-    let timer = setInterval(function () {
-        current += increment;
-        element.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
 
-counter("counter", 0, 63, 2000);
+// function counter(id, start, end, duration) {
+//     let element = document.getElementById(id);
+//     let range = end - start;
+//     let current = start;
+//     let increment = end > start ? 1 : -1;
+//     let stepTime = Math.abs(Math.floor(duration / range));
+//     let timer = setInterval(function () {
+//         current += increment; 
+//         element.innerHTML = current;
+//         if (current == end) {
+//             clearInterval(timer);
+//         }
+//     }, stepTime);
+// }
+
+// counter("counter", 0, 63, 2000);
